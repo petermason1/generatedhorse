d5dd7e39250196601d0530b08fdb750c76bd692f
Peter Mason <peter.masonuk@icloud.com>
2025-07-08 09:22:24 +0100

    Some update

--- tips.js
+++ tips.js
@@ -154,7 +154,7 @@ console.log('tips.js: Script started.');
 
     // === TIPSTER PICKS: ENTER HORSE NAMES FOR EACH TIPSTER (CASE-INSENSITIVE) ===
     const michaelsTips = ["bluebella", "ukantango", "wobwobwob", "billy no mates"];
-    const chrisTips    = ["betty's tiara", "the great escape", "triggerman", "grand harbour"];
+    const chrisTips    = ["amestris", "the vital spark", "voix de bocelli", "semser"];
     const peterTips    = ["taygar", "masonbrook meadow","brave bob", ""];
     const kenTips      = ["we still believe", "binhareer", "lion's house", "sabrimento"];
     const racingPostTips = ["Jesmond Dawn", "Astelia", "I'm A Starman", "Captain Hanley"];
