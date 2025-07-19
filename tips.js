d4672c2b09eea4e010288b09ee2b85c8aca8bb77
Peter Mason <peter.masonuk@icloud.com>
2025-07-19 08:09:25 +0100

    Some update

--- tips.js
+++ tips.js
@@ -78,7 +78,7 @@ console.log('tips.js: Script started.');
 
   // === Tipster picks ===
   const michaelsTips = ["coole cherry", "meehall", "royal musketeer", "insuspense"];
-  const chrisTips    = ["canaria queen", "melek alreeh", "arabian force", "byblos"];
+  const chrisTips    = [""];
   const peterTips = ["Thunderous Love", "Do Bronxs", "Odin Legacy", "Neoma"];
   const kenTips      = ["spirit lead me", "nahraan", "mudamer", "young fire"];
   const racingPostTips = ["Felicity Smoak", "Miss Hathaway", "Golden Handshake", "Catch Cunningham"];
